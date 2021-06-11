52a97e64e550e85b6b2558ff8176e2379bb03428
Prateek Bhardwaj <prateekb1912@gmail.com>
2021-06-11 07:50:13 +0530

    Draw contours on the document

--- main.py
+++ main.py
@@ -28,13 +28,36 @@ def preprocessImage(img):
 
     return imgEros
 
+def getContours(img):
+    contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    
+    maxArea = 0
+    biggest = np.array([])
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+
+        if area > 5000:
+            cv2.drawContours(img, cnt, -1, (255, 0, 0), 3)
+            perimeter = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02*perimeter, True)
+
+            if area > maxArea and len(approx) == 4:
+                biggest = approx
+                maxArea = area
+    
+    return biggest
+
 
 while cv2.waitKey(1) != 27:     # press ESC to break out
     _, frame = src.read()
     frame = cv2.flip(frame, 1)      #flips the camera to work as a mirror
+    frameCnt = frame.copy()         # a copy to draw contours onto
 
     frame = preprocessImage(frame)
-    cv2.imshow("Original", frame)
+    biggest = getContours(frame)
+
+    cv2.imshow("Original", frameCnt)
 
 src.release()
 cv2.destroyAllWindows()
